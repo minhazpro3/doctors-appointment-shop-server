e625a20dff30d254ea5deb1fba664d4dc827f49b
minhazpro3 <minhazpro1@gmail.com>
2022-03-12 00:42:25 +0600

    add products

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-const express = require('express')
-const app = express()
-const ObjectId = require('mongodb').ObjectId;
-app.use(express.json())
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const ObjectId = require("mongodb").ObjectId;
+app.use(express.json());
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 app.use(cors());
-require('dotenv').config();
-
-
-
+require("dotenv").config();
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z45ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
- 
-
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db('doctorAppointment&Shop');
-    const patients = database.collection("patients")
-    const doctors = database.collection("doctors")
+    const database = client.db("doctorAppointment&Shop");
+    const patients = database.collection("patients");
+    const doctors = database.collection("doctors");
+    const allProducts = database.collection("allProducts");
     console.log("connected database");
 
-
-
     // post patients collections
-    app.post("/allPatients", async (req,res)=>{
+    app.post("/allPatients", async (req, res) => {
       const patientsCollection = req.body;
-      const result = await patients.insertOne(patientsCollection)
-      res.send(result)
-    })
+      const result = await patients.insertOne(patientsCollection);
+      res.send(result);
+    });
 
-    // get admin patients 
-    app.get("/getAdminPatients", async (req,res)=>{
-      const result = await patients.find({}).toArray()
-      res.send(result)
-    })
-
-
-    
+    // get admin patients
+    app.get("/getAdminPatients", async (req, res) => {
+      const result = await patients.find({}).toArray();
+      res.send(result);
+    });
 
     // delete patient
-    app.delete('/deleteMySerial/:id', async (req,res)=>{
+    app.delete("/deleteMySerial/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) };
       const result = await patients.deleteOne(query);
       res.send(result);
-  })
+    });
 
 
+    // add products
+    app.post("/addProduct", async (req, res) => {
+      const name = req.body.name;
+      const rating = req.body.rating;
+      const price = req.body.price;
+      const discountPrice = req.body.discountPrice;
+      const discount = req.body.discount;
+      const description = req.body.description;
+      const picture = req.files.image;
+      const pictureData = picture.data;
+      const encodedPicture = pictureData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPicture, "base64");
+      const product = {
+        name,
+        rating,
+        price,
+        discountPrice,
+        discount,
+        description,
+        image: imageBuffer,
+      };
+      const result = await allProducts.insertOne(product);
+      res.send(result);
+    });
 
-
-
-
+    
 
   } finally {
     // await client.close();
@@ -65,10 +82,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log("Connected with database", port)
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Connected with database", port);
+});
