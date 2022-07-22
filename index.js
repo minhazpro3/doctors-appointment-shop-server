4590fd3286e8d998bc2bfe20a0cd6b23f08a17f0
minhazpro3 <minhazpro1@gmail.com>
2022-07-22 16:29:05 +0600

    bug

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ async function run() {
     })
 
 
-    // cancel  order
+    // cancel order
     app.delete("/deleteProd/:id", async (req,res)=>{
       const id = req.params.id;
       const query = {_id:id};
