5fe246c1a22a2610c22dac2509a58807298e3c73
minhazpro3 <minhazpro1@gmail.com>
2022-07-22 21:51:52 +0600

    bug fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const ObjectId = require("mongodb").ObjectId;
 app.use(express.json());
 const cors = require("cors");
-app.use(cors(corsOptions));
+app.use(cors());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -129,7 +129,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await allProducts.deleteOne(query);
-      console.log(result);
       res.send(result);
     });
 
@@ -247,8 +246,9 @@ async function run() {
 
     // update cart
     app.put("/updatePQty", async (req,res)=>{
-      const filter = {_id:req.body._id}
+      const filter = {id:req.body.id}
       const check = req.body.type
+      console.log(req.body);
       const result = await saveCart.find(filter).toArray();
       const upDoc = result[0].qty
       if (result&&!check) {
@@ -273,7 +273,8 @@ async function run() {
     // cancel order
     app.delete("/deleteProd/:id", async (req,res)=>{
       const id = req.params.id;
-      const query = {_id:id};
+      
+      const query = {id:id};
       const result = await saveCart.deleteOne(query)
       res.json(result)
 
